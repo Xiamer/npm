c33279820c3fb107f4a604dbf7f2f5408caf9cce
周晓光 <zhouxg@yiche.com>
2020-04-26 18:56:39 +0800

    init project

---
+++ fe-util/README.md
@@ -0,0 +1,29 @@
+# fe-util 前端常用的js
+
+## 目录
+
+```pre
+.
+├── build # 编译脚本目录
+│   └── rollup.config.js
+├── dist # 编译结果目录
+│   ├── index.js
+│   └── index.js.map
+├── example # HTML使用例子
+│   └── index.html
+├── package.json
+└── src # ES6+ 源码
+    ├── index.js
+    └── lib
+        └── demo.js
+```
+
+
+
+## 常用的rollup
+
+* rollup 模块是rollup.js编译的核心模块
+* rollup-plugin-babel 模块是rollup.js支持babel官方编译插件模块
+* babel-core 是官方 babel 编译核心模块
+* babel-preset-env 是官方 babel 编译解析ES6+ 语言的扩展模块
+* babel-plugin-transform-object-rest-spread 是官方  babel 编译ES6+ 对象扩展的编译插件
---
+++ fe-util/build/rollup.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const babel = require('rollup-plugin-babel');
+const buble = require('rollup-plugin-buble');
+const serve = require('rollup-plugin-serve');
+
+const resolveFile = function(filePath) {
+  return path.join(__dirname, '..', filePath)
+}
+
+module.exports = {
+  input: resolveFile('src/index.js'),
+  output: {
+    file: resolveFile('dist/index.js'),
+    format: 'umd',
+    sourcemap: true, // 开发模式，开启sourcemap文件的生成
+  },
+  plugins: [
+    // 使用和配置babel编译插件
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    // babel({
+    //   "presets": [
+    //     ["env", {
+    //       "modules": false
+    //     }],
+    //   ],
+    //   "plugins": [
+    //     "transform-object-rest-spread",
+    //   ],
+    // }),
+    // 使用开发服务插件
+    // serve({
+    //   port: 3002,
+    //   // 设置 exmaple的访问目录和dist的访问目录
+    //   contentBase: [resolveFile('example'), resolveFile('dist')]
+    // }),
+    buble()
+  ],
+}
